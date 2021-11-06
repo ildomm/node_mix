79c744d7f2f9ff33a4762c05fe4613f5dcb11513
Ildo Grings <ildomm@hotmail.com>
2021-11-06 18:09:08 +1300

    Update api_controller.rb

--- ror_api_server/app/controllers/api_controller.rb
+++ ror_api_server/app/controllers/api_controller.rb
@@ -1,9 +1,5 @@
 class ApiController < ApplicationController
   def find
-    #render :text => 'ok'
-    xxx = BPUpdateWorker.new(  )
-    xxx.perform("http://127.0.0.1:8888/")
-
     element = Bpclient.find_by(params[:key] => params[:value] )
     if element
       element = element.attributes.except("_id")
